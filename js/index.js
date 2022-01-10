ab1c0e5ff7bc4789837d8e860d9ce9ad1020601b
plche <che.percy.l@gmail.com>
2022-01-10 09:33:38 -0500

    Final commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function changeValue(element) {
+    element.innerText = "Logout";
+}
+
+function removeButton(element) {
+    element.remove();
+}
+
+function alertMessage() {
+    alert("Ninja was liked")
+}
